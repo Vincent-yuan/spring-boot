1737a571b814f522c73814f441eb1f8a8691bb9f
yuan.neuq@foxmail.com <yuan.neuq@foxmail.com>
2020-11-16 22:37:58 +0800

    1

--- spring-boot-mybatis-xml-mulidatasource/src/main/java/com/vincent/MybatisApplication.java
+++ spring-boot-mybatis-xml-mulidatasource/src/main/java/com/vincent/MybatisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.vincent.mapper")
 public class MybatisApplication {
 
     public static void main(String[] args) {
